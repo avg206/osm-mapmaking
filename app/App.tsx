5710b1736d55576c45ef29fe33757d315b14cc7d
Anatoli Huseu <anatoli.huseu@gojuno.com>
2019-11-26 16:08:55 +0300

    feat: add types and strcuture

--- app/App.tsx
+++ app/App.tsx
@@ -2,6 +2,11 @@ import React from 'react';
 
 import Map from './components/Map';
 
-const App = () => <Map center={[-104.991531, 39.742043]} />;
+const App = () => (
+  <React.Fragment>
+    <Map center={[-104.991531, 39.742043]} />
+    {/* UI Components here */}
+  </React.Fragment>
+);
 
 export default App;
