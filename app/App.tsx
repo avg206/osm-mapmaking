53d3fb8fceb0344241c8ad9c4a7ab5d8484a5d16
Anatoli Huseu <anatoli.huseu@gojuno.com>
2019-11-26 14:54:22 +0300

    feat: change scripts lib

--- app/App.tsx
+++ app/App.tsx
@@ -2,6 +2,6 @@ import React from 'react';
 
 import Map from './components/Map';
 
-const App = () => <Map center={[23.68775, 52.09755]} />;
+const App = () => <Map center={[-104.991531, 39.742043]} />;
 
 export default App;
---
+++ mocks/routes/index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const { Router } = require('@juno/toolbox/mock-server/utils');
+
+const route = Router();
+
+module.exports = route;
